dad0f7414eb6ce1767cb993a003b56b9a1a89eac
Yalalt <yalaltgandush@gmail.com>
2023-03-20 12:44:43 +0800

    error edit:

---
+++ routes/imageUpload.js
@@ -0,0 +1,4 @@
+import multer from "multer";
+import expres from "express";
+
+// upload router listener
--- server.js
+++ server.js
@@ -9,6 +9,7 @@ import users from "./routes/users.js";
 import categories from "./routes/categories.js";
 import specification from "./routes/specification.js";
 import wishlist from "./routes/wishlist.js";
+// import cloudinary from "./config/cloudinary.js";
 // New create
 // import moderator from "./routes/moderator.js";
 // import orders from "./routes/orders.js";
@@ -24,9 +25,24 @@ app.use("/api", wishlist);
 // app.use("/api", );
 // app.use("/api", );
 
+// const res = cloudinary.v2.uploader.upload(
+//   "https://upload.wikimedia.org/wikipedia/commons/a/ae/Olympic_flag.jpg",
+//   { folder: "test", use_filename: true }
+// );
+
+// res
+//   .then((data) => {
+//     console.log(data);
+//     console.log(data.secure_url);
+//     s;
+//   })
+//   .catch((err) => {
+//     console.log(err);
+//   });
+
 
 app.get("/api", (req, res) => {
-    res.json({ message: "Welcome Electronic shops API"});
-})
+  res.json({ message: "Welcome Electronic shops API" });
+});
 
 app.listen(PORT, () => console.log("Service is runnig port " + PORT));
