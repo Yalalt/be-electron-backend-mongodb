191846a0c3579aa6b69d1dded456dd78013f5f62
Yalalt <yalaltgandush@gmail.com>
2023-03-29 13:38:36 +0800

    some edit

--- controller/category.controller.js
+++ controller/category.controller.js
@@ -1,15 +1,8 @@
-import {
-  addCategory,
-  getCategory,
-  getCategoryLimit,
-  updateCategory,
-  deleteCategory,
-  getOneCat,
-} from "../model/services/category-service.js";
+import { CategoryModel } from "../model/all.model.js";
 
 export const getAll = async (req, res) => {
   try {
-    const result = await getCategory();
+    const result = await CategoryModel.find({});
     res.json({ status: true, result });
   } catch (error) {
     console.log(error);
@@ -19,12 +12,12 @@ export const getAll = async (req, res) => {
 
 export const getOne = async (req, res) => {
     const { id } = req.params;
-    if (!id) return res.json({ status: false, message: "cate not found" });
+    if (!id) return res.json({ status: false, message: "category not found" });
     
     try {
-      const result = await getOneCat(id);
+      // const result = await getOneCat(id);
   
-      res.json({ status: true, result });
+      // res.json({ status: true, result });
     } catch (err) {
       res.json({ status: false, message: err });
     }
@@ -33,8 +26,8 @@ export const getOne = async (req, res) => {
 export const getCatByLimit = async (req, res) => {
   const { query } = req;
   try {
-    const result = await getCategoryLimit(query.cat_no);
-    res.json({ status: true, result });
+    // const result = await getCategoryLimit(query.cat_no);
+    // res.json({ status: true, result });
   } catch (error) {
     console.log(error);
     res.json({ status: false, message: error });
@@ -46,8 +39,8 @@ export const create = async (req, res) => {
   const { name } = req.body;
 
   try {
-    const result = await addCategory(name);
-    res.json({ status: true, result });
+    // const result = await addCategory(name);
+    // res.json({ status: true, result });
   } catch (error) {
     console.log(error);
     res.json({ status: false, message: error });
@@ -55,14 +48,14 @@ export const create = async (req, res) => {
 };
 
 export const updateCat = async (req, res) => {
-  console.log("Create Category huselt");
+  console.log("Update Category huselt");
   const { id } = req.params;
   const { name } = req.body;
   const updateCateData = { categoryId: id, updateData: name };
 
   try {
-    const result = await updateCategory(updateCateData);
-    res.json({ status: true, result });
+    // const result = await updateCategory(updateCateData);
+    // res.json({ status: true, result });
   } catch (error) {
     console.log(error);
     res.json({ status: false, message: error });
@@ -74,13 +67,13 @@ export const deleteCat = async (req, res) => {
   if (!id) return res.json({ status: false, message: "Category not found" });
 
   try {
-    const result = await deleteCategory(categoryId);
+    // const result = await deleteCategory(categoryId);
 
-    if (result && result[0].affectedRows > 0) {
-      return res.json({ status: true, message: "Success" });
-    } else {
-      return res.json({ status: false, message: "Error" });
-    }
+    // if (result && result[0].affectedRows > 0) {
+    //   return res.json({ status: true, message: "Success" });
+    // } else {
+    //   return res.json({ status: false, message: "Error" });
+    // }
   } catch (error) {
     res.json({ status: false, message: err });
   }
